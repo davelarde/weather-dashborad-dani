5f035ec8879f5097df048b480354baf1b8ed5a44
danivelarde <danielavelarde4@gmail.com>
2022-04-12 18:35:54 -0400

    changes

---
+++ script.js
@@ -0,0 +1,8 @@
+var apiKey= "b2317bf20aeba7c04751bf8262581fc1";
+console.log("hello!")
+var searchBtn = $(".searchBtn");
+var searchInput = $(".searchInput");
+
+
+var cityNameEl = $(".cityName");
+var currentDateEl = $("currentDate");
